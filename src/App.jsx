b106a4a8af8d69a9a6ba76337ded5d5e669648a9
rasul <janrasula.2011@gmail.com>
2026-07-09 17:16:45 +0600

    added playlist

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Bro from "./bro/Bro";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import TrendingSongs from "./box/Box";
 
 function App() {
   return (
     <div>
       <Header />
       <Bro />
+      <TrendingSongs />
       <Footer />
     </div>
   );
